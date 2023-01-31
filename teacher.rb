3bd79518d68952165574e868f4484bc6843b3170
jonathanmunamire <jonathanmunamire@gmail.com>
2023-01-31 10:13:49 +0200

    Add Teacher class

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require_relative './person'
+
+class Teacher < Person
+    def initialize (name : "unknown", age, parent_permission : true, specialization)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        true
+    end
+end
