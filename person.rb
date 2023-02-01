c08ac479b81c723e8b1fd2273fc50c1596be18bf
jonathanmunamire <jonathanmunamire@gmail.com>
2023-02-01 12:13:10 +0200

    Add Decoration Class

--- person.rb
+++ person.rb
@@ -1,6 +1,12 @@
-class Person
-  def initialize(age, name = 'unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Person < Nameable
+  def initialize(age, name, parent_permission: true)
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -18,6 +24,31 @@ class Person
   end
 
   private :of_age?
+
+  def correct_name
+    @name
+  end
+
 end
 
-Person.new(14, 'Jonathan')
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.chars.slice(0, 9).join
+  end
+end
