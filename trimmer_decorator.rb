bbc021ed4f3dadbed8fc7c29a29b4813af92a6fd
jonathanmunamire <jonathanmunamire@gmail.com>
2023-02-09 15:10:48 +0200

    Fix Linters

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,8 +1,7 @@
 require './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
-    def correct_name
-      @nameable.correct_name.chars.slice(0, 10).join
-    end
+  def correct_name
+    @nameable.correct_name.chars.slice(0, 10).join
   end
-  
+end
